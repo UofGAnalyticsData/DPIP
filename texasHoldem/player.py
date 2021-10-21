4e6487204aa915efed83177860f83a33f7ef4676
andeElliott <ande.elliott@gmail.com>
2021-10-21 12:09:10 +0100

    bug fix

--- texasHoldem/player.py
+++ texasHoldem/player.py
@@ -2,10 +2,12 @@
 
 class playerBase:
     id_number = 0
-    def __init__(self):
+    def __init__(self,name=None):
         self.cards = []
         self.id_number = playerBase.id_number
         playerBase.id_number += 1
+        if name!=None:
+            self.name = name
     def takeCard(self,card):
         self.cards.append(card)
     def getCards(self):
@@ -14,13 +16,13 @@ class playerBase:
         self.cards = []
 
 class player_bet1(playerBase):
-    def chooseBet(self,*args):
-        return 1
+    def chooseBet(self,minBetToStay,curComCards,curBets,idx):
+        return max(1,minBetToStay)
 
 
 class player_onlyHighCard(playerBase):
     def chooseBet(self,*args):
         nums = [x.getNumber() for x in self.cards]
         if nums[0] in ['10','Jack','Queen','King','Ace'] or nums[1] in ['10','Jack','Queen','King','Ace']:
-                return 1
+                return max(1,minBetToStay)
         return 0
